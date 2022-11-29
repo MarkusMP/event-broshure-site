c2c9145ef5a53ad8c41201b55e4a9d017c4e6ab4
Markus Persson <markusperssonbiz@gmail.com>
2022-11-29 19:39:59 +0100

    add hero animation, scroll to top button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 type Props = {
   children?: ReactNode;
@@ -9,10 +10,39 @@ type Props = {
   footer?: any;
 };
 export default function Layout({ children, header, footer }: Props) {
+  const [show, setShow] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 200) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+
+  const handleGoTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <>
       <Header data={header} />
-      <div className="flex flex-col">
+      <div className="flex flex-col relative">
+        <button
+          onClick={handleGoTop}
+          className={`flex items-center ${
+            !show ? "hidden" : ""
+          } justify-center fixed z-90 bottom-8 right-8 border-0 w-14 h-14 rounded-md drop-shadow-md bg-primary hover:bg-secondary dark:bg-secondary hover:dark:bg-primary transition-all text-white text-3xl font-bold`}
+        >
+          <AiOutlineArrowUp />
+        </button>
         <main className="flex-grow">{children}</main>
       </div>
       <Footer data={footer} />
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = ({ title, image, description }: IHero) => {
         fill
         priority
       />
-      <div className="lg:container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between">
+      <div className="lg:container slide relative flex flex-wrap items-center justify-center p-8 mx-auto">
         <div className="bg-white bg-opacity-90 rounded-md mx-auto p-8 text-center dark:bg-dark dark:bg-opacity-90 block">
           <h1 className="font-bold tracking-wide text-2xl md:text-4xl pb-2 break-words w-full inline-block">
             {title && title}
